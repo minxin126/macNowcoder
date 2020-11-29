58f0d28a31eb7cb73fa4bad6d3a88d3547cd7679
minxin126 <wangminxin6@126.com>
2020-11-29 09:29:07 +0800

    speak some words.

---
+++ cmake-build-debug/CMakeFiles/macNowcoder.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/macNowcoder.dir/main.cpp.o"
+  "macNowcoder"
+  "macNowcoder.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/macNowcoder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
